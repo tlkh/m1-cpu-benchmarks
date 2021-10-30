e243a2ae256857a533701a3b1b4c1bab0ccb6ac4
Timothy Liu <tlkh.xms@gmail.com>
2021-10-30 17:58:36 +0800

    update jax benchmark

--- jax_benchmarks.py
+++ jax_benchmarks.py
@@ -5,7 +5,7 @@ from jax import random
 key = random.PRNGKey(0)
 
 size = 4096
-iterations = 100
+iterations = 60
 
 mat = random.normal(key, (size, size))
 mat_T = mat.T
@@ -14,6 +14,8 @@ def apply_matrix(v):
     return jnp.dot(mat, v)
 
 apply_matrix(mat_T).block_until_ready()
+
+time.sleep(1)
 
 st = time.time()
 for i in range(iterations):
@@ -25,11 +27,15 @@ fps = iterations/duration
 matmul_flops = 2 * (size**3)
 TFLOPS = fps*matmul_flops/(1e12)
 
-print(TFLOPS)
+print("| MatMul |", TFLOPS, "|")
+
+time.sleep(1)
 
 apply_matrix_jit = jit(apply_matrix)
 
 apply_matrix_jit(mat_T).block_until_ready()
+
+time.sleep(1)
 
 st = time.time()
 for i in range(iterations):
@@ -41,25 +47,11 @@ fps = iterations/duration
 matmul_flops = 2 * (size**3)
 TFLOPS = fps*matmul_flops/(1e12)
 
-print(TFLOPS)
+print("| JIT MatMul |", TFLOPS, "|")
+
+time.sleep(1)
 
 batched_x = random.normal(key, (iterations, size, size))
-
-def naively_batched_apply_matrix(v_batched):
-    return jnp.stack([apply_matrix(v) for v in v_batched])
-
-naively_batched_apply_matrix(batched_x).block_until_ready()
-
-st = time.time()
-naively_batched_apply_matrix(batched_x).block_until_ready()
-et = time.time()
-
-duration = et-st
-fps = iterations/duration
-matmul_flops = 2 * (size**3)
-TFLOPS = fps*matmul_flops/(1e12)
-
-print(TFLOPS)
 
 @jit
 def vmap_batched_apply_matrix(v_batched):
@@ -67,6 +59,8 @@ def vmap_batched_apply_matrix(v_batched):
 
 vmap_batched_apply_matrix(batched_x).block_until_ready()
 
+time.sleep(1)
+
 st = time.time()
 vmap_batched_apply_matrix(batched_x).block_until_ready()
 et = time.time()
@@ -76,4 +70,4 @@ fps = iterations/duration
 matmul_flops = 2 * (size**3)
 TFLOPS = fps*matmul_flops/(1e12)
 
-print(TFLOPS)
+print("| JIT+VMAP MatMul |", TFLOPS, "|")
